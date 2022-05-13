6c786a662fdbdc1b808acbddbeed1e7111394ad1
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2022-05-13 13:49:15 +0200

    wip

--- CamarasDgt/api/admin.py
+++ CamarasDgt/api/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from api.Cameras_Madrid.models import  CamerasMadrid
 from api.Cameras_Cat.models import  CamerasCat
-from api.Cameras_Valladolid import CamerasValladolid
+from api.Cameras_Valladolid.models import CamerasValladolid
 from api.Cameras_Zaragoza.models import  CamerasZaragoza
 from api.Cameras_Valencia.models import CamerasValencia
-from.api.Cameras_Malaga.models import CamerasMalaga
+from api.Cameras_Malaga.models import CamerasMalaga
 from api.Cameras_Coruña.models import CamerasCoruña
-from.api.Camaras_Sevilla.models import CamerasSevilla
-from.api.Camaras_Baleares.models import CamerasBaleares
+from api.Cameras_Sevilla.models import CamerasSevilla
+from api.Cameras_Baleares.models import CamerasBaleares
 
 admin.site.register(CamerasMadrid)
 admin.site.register(CamerasCat)
--- CamarasDgt/api/urls.py
+++ CamarasDgt/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from api.Cameras_Madrid.views import CamerasMadrid_APIView, Camerasadrid_APIView_Detail
+from api.Cameras_Madrid.views import CamerasMadrid_APIView, CamerasMadrid_APIView_Detail
 from api.Cameras_Cat.views import CamerasCat_APIView, CamerasCat_APIView_Detail
-from api.Camaras_Valladolid.views import CamerasValladolid_APIView, CamerasValladolid_APIView_Detail
-from api.Camaras_Zaragoza.views import CamerasZaragoza_APIView, CamerasZaragoza_APIView_Detail
-from api.Camaras_Valencia.views import CamerasValencia_APIView, CamerasValencia_APIView_Detail
-from api.Camaras_Malaga import CamerasMalaga_APIView, CamerasMalaga_APIView_Detail
-from api.Camaras_Sevilla.views import CamerasSevilla_APIView, CamerasSevilla_APIView_Detail
-from api.Camaras_Baleares.views import CamerasBaleares_APIView, CamerasBaleares_APIView_Detail
+from api.Cameras_Valladolid.views import CamerasValladolid_APIView, CamerasValladolid_APIView_Detail
+from api.Cameras_Zaragoza.views import CamerasZaragoza_APIView, CamerasZaragoza_APIView_Detail
+from api.Cameras_Valencia.views import CamerasValencia_APIView, CamerasValencia_APIView_Detail
+from api.Cameras_Malaga.views import CamerasMalaga_APIView, CamerasMalaga_APIView_Detail
+from api.Cameras_Sevilla.views import CamerasSevilla_APIView, CamerasSevilla_APIView_Detail
+from api.Cameras_Baleares.views import CamerasBaleares_APIView, CamerasBaleares_APIView_Detail
 
 
 
@@ -19,14 +19,14 @@ urlpatterns = [
     path('v1/CamerasCat/<int:pk>/', CamerasCat_APIView_Detail.as_view()),
     path('v1/CamerasValladolid/', CamerasValladolid_APIView.as_view()),
     path('v1/CamerasValladolid/<int:pk>/', CamerasValladolid_APIView_Detail.as_view()),
-    path('v1/CamarasZaragoza/', CamerasZaragoza_APIView.as_view()),
-    path('v1/CamarasZaragoza/<int:pk>/', CamerasZaragoza_APIView_Detail.as_view()),
-    path('v1/CamarasValencia/', CamerasValencia_APIView.as_view()),
-    path('v1/CamarasValencia/<int:pk>/', CamerasValencia_APIView_Detail.as_view()),
-    path('v1/CamarasMalaga/', CamerasMalaga_APIView.as_view()),
-    path('v1/CamarasMalaga/<int:pk>/', CamerasMalaga_APIView_Detail.as_view()),
-    path('v1/CamarasSevilla/', CamerasSevilla_APIView.as_view()),
-    path('v1/CamarasSevilla/<int:pk>/', CamerasSevilla_APIView_Detail.as_view()),
-    path('v1/CamarasBaleares/', CamerasBaleares_APIView.as_view()),
-    path('v1/CamarasBaleares/<int:pk>/', CamerasBaleares_APIView_Detail.as_view()),
+    path('v1/CamerasZaragoza/', CamerasZaragoza_APIView.as_view()),
+    path('v1/CamerasZaragoza/<int:pk>/', CamerasZaragoza_APIView_Detail.as_view()),
+    path('v1/CamerasValencia/', CamerasValencia_APIView.as_view()),
+    path('v1/CamerasValencia/<int:pk>/', CamerasValencia_APIView_Detail.as_view()),
+    path('v1/CamerasMalaga/', CamerasMalaga_APIView.as_view()),
+    path('v1/CamerasMalaga/<int:pk>/', CamerasMalaga_APIView_Detail.as_view()),
+    path('v1/CamerasSevilla/', CamerasSevilla_APIView.as_view()),
+    path('v1/CamerasSevilla/<int:pk>/', CamerasSevilla_APIView_Detail.as_view()),
+    path('v1/CamerasBaleares/', CamerasBaleares_APIView.as_view()),
+    path('v1/CamerasBaleares/<int:pk>/', CamerasBaleares_APIView_Detail.as_view()),
 ]
